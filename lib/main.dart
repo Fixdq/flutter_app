2f7c34c7778f4b5d4bee0b9f6f7b54959bf2f2ee
fixdq <fixd.quan@aliyun.com>
2019-07-20 12:51:20 +0800

    modify:fluro初始化配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
 import 'dart:async';
 
+import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_app/common/config/config.dart';
 import 'package:flutter_app/common/local/local_storage.dart';
 import 'package:flutter_app/common/model/user.dart';
 import 'package:flutter_app/common/net/code.dart';
 import 'package:flutter_app/common/redux/global_state.dart';
+import 'package:flutter_app/common/router/application.dart';
+import 'package:flutter_app/common/router/routers.dart';
 import 'package:flutter_app/common/style/global_style.dart';
 import 'package:flutter_app/common/utils/common_utils.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:redux/redux.dart';
 
 import 'common/event/event_instance.dart';
 import 'common/event/event_model.dart';
-import 'pages/page_home.dart';
 
 void main() async {
   runApp(MyApp());
@@ -34,7 +38,14 @@ class MyApp extends StatelessWidget {
   MyApp({
     Key key,
   }) : super(key: key) {
-    print('start');
+    ///fluro 路由
+    final router = Router();
+
+    /// 绑定路由
+    Routers.configureRouters(router);
+    Application.router = router;
+
+    /// 主题设置
     getThemeColorIndex(this.store);
 
     /// 初始化 event 监听
@@ -57,10 +68,15 @@ class MyApp extends StatelessWidget {
         store: store,
         child: StoreBuilder<GlobalState>(builder: (context, store) {
           return MaterialApp(
-              theme: store.state.themeData,
-              routes: {
-                HomePage.sName: (context) {return HomePage();},
-          });
+            /// 设置主题
+            theme: store.state.themeData,
+
+            /// 关闭debug标志
+            debugShowCheckedModeBanner: false,
+
+            /// 绑定路由
+            onGenerateRoute: Application.router.generator,
+          );
         }));
   }
 
@@ -78,14 +94,15 @@ class MyApp extends StatelessWidget {
       case 404:
         Fluttertoast.showToast(msg: "网络错误");
         break;
+      case 10001:
+        Fluttertoast.showToast(msg: "路由不正确");
+        break;
       case Code.NETWORK_TIMEOUT:
         //超时
         Fluttertoast.showToast(msg: "网络错误");
         break;
       default:
-        Fluttertoast.showToast(
-            fontSize:12.0,
-            msg: "$message");
+        Fluttertoast.showToast(fontSize: 12.0, msg: "$message");
         break;
     }
   }
