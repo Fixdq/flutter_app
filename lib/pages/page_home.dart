4590aebf73ac65c4d0f7557421f8a91e30a0d7c1
fixdq <fixd.quan@aliyun.com>
2019-07-28 09:25:50 +0800

    modify:首页初始化路由.

--- lib/pages/page_home.dart
+++ lib/pages/page_home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/common/utils/navigator_utils.dart';
-import 'package:flutter_app/pages/page_login.dart';
-import 'package:flutter_app/pages/page_theme.dart';
+import 'package:flutter_app/common/router/application.dart';
+import 'package:flutter_app/common/router/routers.dart';
 
 class HomePage extends StatelessWidget {
   static final String sName = '/';
@@ -16,24 +15,45 @@ class HomePage extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(5.0),
         children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.ac_unit),
-            title: Text('登录'),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: (){
-              NavigatorUtils.NavigatorRouter(context,LoginPage());
-            },
+          MyListTile(
+            icon:Icons.ac_unit,
+            title: '登录',
+            path: Routers.login,
           ),
-          ListTile(
-            leading: Icon(Icons.open_with),
-            title: Text('主题'),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: (){
-              NavigatorUtils.NavigatorRouter(context,ThemeChange());
-            },
+          MyListTile(
+            icon:Icons.open_with,
+            title: '主题',
+            path: Routers.theme,
+          ),
+          MyListTile(
+            icon:Icons.camera,
+            title: 'ListView默认',
+            path: Routers.listview,
           ),
         ],
       ),
+    );
+  }
+}
+
+
+class MyListTile extends StatelessWidget{
+
+  final IconData icon;
+  final String title;
+  final String path;
+
+  MyListTile({this.icon, this.path, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      trailing: Icon(Icons.keyboard_arrow_right),
+      onTap: (){
+        Application.router.navigateTo(context, path);
+      },
     );
   }
 }
