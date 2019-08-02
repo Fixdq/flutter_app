6defccab946ef29e741945e05e50663f78f21eaa
fixdq <fixd.quan@aliyun.com>
2019-08-02 15:22:50 +0800

    add:ListView实例的Home页.

---
+++ lib/pages/page_listview.dart
@@ -0,0 +1,54 @@
+import 'package:fluro/fluro.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/common/router/application.dart';
+import 'package:flutter_app/common/router/routers.dart';
+
+class FListViewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ListView"),
+      ),
+      body: FixdListView(),
+    );
+  }
+}
+
+class FixdListView extends StatefulWidget {
+  @override
+  _FixdListViewState createState() => _FixdListViewState();
+}
+
+class _FixdListViewState extends State<FixdListView> {
+  final Map<String, String> listTitles = {
+    '基本使用': Routers.listviewbase,
+    '下拉刷新': Routers.listviewpull,
+    '上拉加载': Routers.listviewload,
+  };
+
+  /// 获取所有item 元素
+  List<Widget> getChilds(Map<String, String> titles) {
+    List<Widget> childs = [];
+
+    /// 遍历生成item
+    titles.forEach((title, path) {
+      childs.add(ListTile(
+        title: Text(title),
+        onTap: () {
+          Application.router.navigateTo(context, path);
+        },
+      ));
+    });
+    return childs;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(10.0),
+      itemExtent: 50.0,
+      children: getChilds(listTitles),
+    );
+  }
+}
